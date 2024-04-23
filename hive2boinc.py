dbd7c866b2e0f5a46fb93621b4c65418ef17c816
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-04-23 22:40:51 +0800

    separate stdout & stderr

--- hive2boinc.py
+++ hive2boinc.py
@@ -8,51 +8,55 @@ MYSQL_USER = "boincadm"
 MYSQL_PASS = "boincpass"
 MYSQL_DB = "test"
 MYSQL_APP = "hive_test"
+RESULTS_DIR = "hive_results"
 
 # script
-async def sh(cmd) -> tuple[list[str], int]:
+async def sh(cmd) -> tuple[str, str, int]:
     p = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.STDOUT,
+        stderr=asyncio.subprocess.PIPE,
     )
 
-    out = []
+    out = err = ""
     async for line in p.stdout:
-        decoded = line.decode().strip()
+        decoded = line.decode()
         print(decoded, end='')
-        out += [decoded]
+        out += decoded
+    async for line in p.stderr:
+        decoded = line.decode()
+        print(decoded, end='')
+        err += decoded
 
-    return out, await p.wait()
+    return out, err, await p.wait()
 
 def mysql_run(cmd):
     return asyncio.run(sh(f"mysql -u {MYSQL_USER} -p{MYSQL_PASS} {MYSQL_DB} -e '{cmd}'"))
 
 if __name__ == '__main__':
     print("running hive job")
-    uuid = uuid.uuid4()
-    out, code = mysql_run("""
+    id = uuid.uuid4()
+    out, err, code = mysql_run("""
                           insert into workunit (
                             create_time, appid, name, batch, 
                             rsc_fpops_est, rsc_fpops_bound, rsc_memory_bound, rsc_disk_bound, rsc_bandwidth_bound, 
                             need_validate, canonical_resultid, canonical_credit, transition_time, delay_bound, error_mask, file_delete_state, assimilate_state, hr_class, opaque, 
                             min_quorum, target_nresults, max_error_results, max_total_results, max_success_results, 
-                            result_template_file, priority, mod_time, fileset_id, app_version_id, transitioner_flags, size_class, keywords, app_version_num
+                            result_template_file, priority, fileset_id, app_version_id, transitioner_flags, size_class, keywords, app_version_num
                           )
-                          
                           values (
-                            UNIX_TIMESTAMP(), (select id from app where name = '{MYSQL_APP}'), 'hive_{uuid}', 0, 
+                            UNIX_TIMESTAMP(), (select id from app where name = '{MYSQL_APP}'), 'hive_{id}', 0, 
                             0.0, 0.0, 0.0, 0.0, 0.0, 
                             0, 0, 0.0, 0, 0, 0, 0, 0, 0, 0.0, 
                             1, 1, 1, 1, 1, 
-                            '', 0, 0, 0, 0, 0, 0, 'hive', 1
+                            '', 0, 0, 0, 0, 0, 'hive', 1
                           )""")
-    out, code = asyncio.run(sh(f"hive run {" ".join(sys.argv[1:])}"))
+    out, err, code = asyncio.run(sh(f"hive run {" ".join(sys.argv[1:])}"))
 
     if "✅  Results accepted. Downloading result..." in out:
         # TODO make less fragile; maybe implement json-mode for hive run
-        ipfs_addr = out[-1].split("/")[-1]
-        res_dir = out[-4].split(" ")[-1]
+        ipfs_addr = out.splitlines()[-1].split("/")[-1]
+        res_dir = out.splitlines()[-4].split(" ")[-1]
         res_out, res_err = f"{res_dir}/stdout", f"{res_dir}/stderr"
 
         print("job succeeded")
